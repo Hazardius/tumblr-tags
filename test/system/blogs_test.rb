78811ab7d4603bfe64c99a9761c9334f70cc0d53
renelt <renataeltman@gmail.com>
2018-01-28 22:21:46 +0100

    Added stubs for system tests.

--- test/system/blogs_test.rb
+++ test/system/blogs_test.rb
@@ -6,4 +6,15 @@ class BlogsTest < ApplicationSystemTestCase
   #
   #   assert_selector "h1", text: "Blog"
   # end
+  
+  test "Anyone can get list of tags for existing blog." do
+    # blog_hehe = FactoryBot.create(:blog)
+    # 1. Open get_tags page with blog identifier. 
+    # visit get_tags_url(blog_hehe)
+    # 2. See list of all tags with links to their subpages.
+
+    # visit blogs_url
+  
+    # assert_selector "h1", text: "Blog"
+  end
 end
