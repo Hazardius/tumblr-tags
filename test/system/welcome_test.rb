c0b933b73d870ba382ed38816514cf84aaa22709
renelt <renataeltman@gmail.com>
2018-02-19 20:08:51 +0100

    Added stubs of system tests.

--- test/system/welcome_test.rb
+++ test/system/welcome_test.rb
@@ -1,9 +1,37 @@
 require "application_system_test_case"
 
 class WelcomeTest < ApplicationSystemTestCase
-  test "visiting the main page" do
+  test "user views the main page" do
+    # 1. Open the main page.
     visit root_url
 
-    assert_selector "div", text: "Welcome inside user!"
+    # 2. Page consists of: header and simple form with a button.
+    assert_selector "h1",     text: "Welcome inside user!"
+    assert_selector "button", text: "Show tags"
+  end
+
+  test "user views tags" do
+    # 1. Open the main page.
+    visit root_url
+
+    # 2. Page has a simple form with a button.
+    assert_selector "button", text: "Show tags"
+
+    # 3. Input a valid blog url or name into form.
+    # 4. Click the button.
+    # 5. Check if we ended up on a right subpage.
+    # 6. We should see tags, links and frequency.
+  end
+
+  test "user tries to view tags for invalid blog" do
+    # 1. Open the main page.
+    visit root_url
+
+    # 2. Page has a simple form with a button.
+    assert_selector "button", text: "Show tags"
+
+    # 3. Input a invalid blog url or name into form.
+    # 4. Click the button.
+    # 5. User sees a flash message "Ooop! Can't find this blog.".
   end
 end
